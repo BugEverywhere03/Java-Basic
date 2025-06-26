1e7ed7d1d88ea36b48477aa80b67ee9fd95c0280
Nguyễn Duy Dương <duongdevpython@gmail.com>
2025-06-26 19:29:01 +0700

    update PrintAndVariables file

---
+++ src/com/ndd/buoi3/DataTypes.java
@@ -0,0 +1,9 @@
+package com.ndd.buoi3;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		System.out.println("Dòng 3");
+		System.out.println("Dòng 4");
+	}
+
+}
---
+++ src/com/ndd/buoi3/PrintAndVariables.java
@@ -0,0 +1,12 @@
+package com.ndd.buoi3;
+
+public class PrintAndVariables {
+	public static void main(String[] args) {
+		// In ra màn hình console và không xuống dòng mới
+		System.out.print("Hello Java ");
+		System.out.print("Dòng 2");
+		// Lệnh prinln() sẽ in ra màn hình console rồi xuống dòng mới
+		System.out.println("Dòng 3");
+		System.out.println("Dòng 4");
+	}
+}
